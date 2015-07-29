12dd9f324edf600e9966fa8683fb03bed91fc3fa
Leonardo Loyo <cyberedward@gmail.com>
2015-07-29 13:42:38 -0430

    sidebard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,37 +37,41 @@
 </head>
 <body>
 	<nav class="navbar colornav text-blanco" >
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<span class="navbar-brand">Transbarca | <small>Sistema de Control y Gestion De Fallas</small></span>
-			</div>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <span class="navbar-brand">Transbarca | <small>Sistema de Control y Gestion De Fallas</small></span>
+            </div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="{{ url('/home') }}" class="text-blanco">Inicio</a></li>
-				</ul>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ url('/home') }}" class="text-blanco">Inicio</a></li>
+                </ul>
 
-				
-				<ul class="nav navbar-nav navbar-right">
-					@if (!Auth::guest())
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle text-blanco" data-toggle="dropdown" role="button" aria-expanded="false">Bienvenido {{ Auth::user()->persona->nombrecompleto }}<span class="caret"></span</a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('changepassword') }}">Cambiar Contraseña</a></li>
-								<li><a href="{{ url('/auth/logout') }}">Cerrar Sesion</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                        
+                        <!--
+                        <li><a href="{{ url('/auth/register') }}">Registrarse</a></li>
+                        -->
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle text-blanco" data-toggle="dropdown" role="button" aria-expanded="false">Bienvenido {{ Auth::user()->rol->descripcion }} - {{ Auth::user()->persona->nombrecompleto }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('changepassword') }}">Cambiar Contraseña</a></li>
+                                <li><a href="{{ url('/auth/logout') }}">Cerrar Sesion</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
 <div class="col-md-12">
 	<div class="col-md-3">
 		@yield('sidebar')
@@ -80,13 +84,13 @@
 	<!-- Scripts -->
 
 
-	
+	{!! Html::script('bower_components/jquery/dist/jquery.min.js') !!}
 	{!! Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
 	{!! Html::script('bower_components/bootstrap-material-design/dist/js/ripples.min.js') !!}
 	{!! Html::script('bower_components/bootstrap-material-design/dist/js/material.min.js') !!}
-	
-	{!! Html::script('bower_components/jquery/dist/jquery.min.js') !!}
 	{!! Html::script('js/sidebar.js') !!}
+	
+	
 	
 	{!! Html::script('materialize/js/materialize.min.js') !!}
 	
--- resources/views/tools/sidebar.blade.php
+++ resources/views/tools/sidebar.blade.php
@@ -3,48 +3,12 @@
     <div class='sidebar-menu'><i class="fa fa-car left"></i>Unidades<div class='expand'><i class="fa fa-angle-left"></i></div></div>
       <div class='sub-menu'>
     <ul>
-    <a href="{{ url('unidades/create') }}"><li>Registrar Unidad</li></a>
-  	<a href="{{ url('unidades/') }}"><li>Consultar Unidades</li></a>
-  	<a href="{{ url('unidadoperadores') }}"><li>Asignar Unidad</li></a>
+    @foreach(Auth::user()->rol->rolopcion as $key)
+              <li>{{ $key->opcion->descripcion }}</li>   
+              @endforeach
     </ul>
-      </div>
-    <div class='sidebar-menu'><i class="fa fa-exclamation-triangle left"></i></i>Fallas<div class='expand'><i class="fa fa-angle-left"></i></div></div>
-      <div class='sub-menu'>
-        <ul>
-        <a href="{{ route('fallas.create') }}"><li>Nueva Falla</li></a>
-        <a href="{{ route('tipos.create') }}"><li>Nuevo Tipo de Falla</li></a>
-        <a href="{{ route('categorias.create') }}"><li>Nueva Categoria de Falla</li></a>
-        <a href="{{ url('rfuoperador') }}"><li>Registrar Falla</li></a>
-        
-       
-        </ul>
-      </div>
-    <div class='sidebar-menu'><i class="fa fa-user  left"></i>Operadores<div class='expand'><i class="fa fa-angle-left"></i></div></div>
-      <div class='sub-menu'>
-      <ul>
-      <a href="{{ url('operadores/create') }}"><li>Registrar Operador</li></a>
-      <a href="{{ url('operadores/') }}"><li>Operadores</li></a>
-      </ul>
-      </div>
-    <div class='sidebar-menu' ><i class="fa fa-file-pdf-o left"></i>Reportes<div class='expand'><i class="fa fa-angle-left"></i></div></div>
-      <div class='sub-menu'>
-      <ul>
-      <a href="{{ url('reportes') }}"><li>Unidades</li></a>
-      <a href="{{ route('reportes.create') }}"><li>Incidencias</li></a>
-      
-      
-      </ul>
-      </div>
-    <div class='sidebar-menu'><i class="mdi-action-settings left"></i>Configuraciones<div class='expand'><i class="fa fa-angle-left"></i></div></div>
-      <div class='sub-menu'>
-      <ul>
-      <a href="{{ url('rusuario') }}"><li>Registrar Usuario</li></a>
-      
-      
-
-
-
-      </ul>
+     
+     
       </div>
   </div>
 @endsection
