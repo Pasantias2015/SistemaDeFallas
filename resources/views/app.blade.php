9eb803a848b6fc705969d1ad270b7d5c12a79f7a
Leonardo Loyo <cyberedward@gmail.com>
2015-08-02 20:38:23 -0430

    foto

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,21 +5,23 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Transbarca</title>
-{!! Html::script('js/jquery-1.11.3.min.js') !!}	
-<!--
-{!! Html::style('materialize/css/materialize.css') !!}
-{!! Html::style('bower_components/bootstrap-material-design/dist/css/material.min.css') !!}
-{!! Html::style('bower_components/bootstrap-material-design/dist/css/ripples.min.css') !!}
-{!! Html::style('bower_components/bootstrap-material-design/dist/css/roboto.min.css') !!}
-{!! Html::style('bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css') !!}
-{!! Html::script('bower_components/bootstrap-material-design/dist/js/ripples.min.js') !!}
+		
+	<!--
+	{!! Html::style('materialize/css/materialize.css') !!}
+	{!! Html::style('bower_components/bootstrap-material-design/dist/css/material.min.css') !!}
+	{!! Html::style('bower_components/bootstrap-material-design/dist/css/ripples.min.css') !!}
+	{!! Html::style('bower_components/bootstrap-material-design/dist/css/roboto.min.css') !!}
+	{!! Html::style('bower_components/bootstrap-material-design/dist/css/material-fullpalette.min.css') !!}
+	{!! Html::script('bower_components/bootstrap-material-design/dist/js/ripples.min.js') !!}
 	{!! Html::script('bower_components/bootstrap-material-design/dist/js/material.min.js') !!}
--->
-    {!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
-
-{!! Html::style('css/style.css') !!}
-
-	<link rel="stylesheet"  href="{!! url('css/app.css') !!}"/>
+	
+	{!! Html::style('css/app.css') !!}
+	-->
+	
+	{!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
+	{!! Html::style('css/style.css') !!}
+	
+	
 	
 
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
@@ -30,14 +32,7 @@
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-	
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
 <body>
 	<nav class="navbar colornav text-blanco" >
@@ -89,11 +84,10 @@
 	
 	
 	<!--Morris Charts-->
-	
+	{!! Html::script('js/jquery-1.11.3.min.js') !!}
+	{!! Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
 	{!! Html::script('js/raphael-min.js') !!}
 	{!! Html::script('js/morris.min.js') !!}
-	{!! Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
-
 	{!! Html::script('js/sidebar.js') !!}
 
 
